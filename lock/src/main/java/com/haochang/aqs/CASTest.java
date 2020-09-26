29035e5a918eee566485ed3fd1915cb0fe546030
youzhi.gao <gyz0604@163.com>
2020-09-26 08:30:24 +0800

    lock测试

---
+++ lock/src/main/java/com/haochang/aqs/CASTest.java
@@ -0,0 +1,27 @@
+package com.haochang.aqs;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CASTest {
+    private static int sum = 0;
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        for (int i =0;i < 3; i++){
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        lock.lock();
+                        sum++;
+                    }finally {
+                        lock.unlock();
+                    }
+
+                }
+            });
+            thread.start();
+        }
+    }
+}
