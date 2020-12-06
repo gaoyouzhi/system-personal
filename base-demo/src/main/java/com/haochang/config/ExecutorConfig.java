621e8d7c3bf10517ac5d3eb35c674ded6d951dbe
youzhi.gao <gyz0604@163.com>
2020-12-06 21:03:43 +0800

    Merge branch 'master' of https://github.com/gaoyouzhi/system-personal
    # Conflicts:
    #	spring-haochang/src/main/java/com/haochang/spring/config/AppConfig.java
    #	spring-haochang/src/main/java/com/haochang/spring/model/User.java

--- base-demo/src/main/java/com/haochang/config/ExecutorConfig.java
+++ base-demo/src/main/java/com/haochang/config/ExecutorConfig.java
@@ -1,20 +1,20 @@
-package com.haochang.config;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.context.annotation.Bean;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
-
-/**
- * @author Administrator
- */
-//@Configuration
-//@EnableAsync
-public class ExecutorConfig {
-
-    private static final Log log = LogFactory.getLog(ExecutorConfig.class);
-
-
-}
+//package com.haochang.config;
+//
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+//import org.springframework.context.annotation.Bean;
+//
+//import java.util.concurrent.Executor;
+//import java.util.concurrent.ThreadPoolExecutor;
+//
+///**
+// * @author Administrator
+// */
+////@Configuration
+////@EnableAsync
+//public class ExecutorConfig {
+//
+//    private static final Log log = LogFactory.getLog(ExecutorConfig.class);
+//
+//
+//}
